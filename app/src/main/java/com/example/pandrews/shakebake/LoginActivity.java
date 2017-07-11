564f118edefd0c4b5c2b74313ec803cbb9c6beda
Parker Andrews <pandrews@fb.com>
2017-07-11 11:25:38 -0700

    added stuff

--- app/src/main/java/com/example/pandrews/shakebake/LoginActivity.java
+++ app/src/main/java/com/example/pandrews/shakebake/LoginActivity.java
@@ -1,9 +1,10 @@
 package com.example.pandrews.shakebake;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -14,14 +15,11 @@ public class LoginActivity extends AppCompatActivity {
     }
 
     public void onLoginPressed (View v) {
-//        EditText etEmail = (EditText) findViewById(R.id.email);
-//        v.setEnabled(false);
-//        String itemText = etEmail.getText().toString();
-//        Toast.makeText(getApplicationContext(), itemText + " logged in", Toast.LENGTH_SHORT).show();
-
-        Intent intent = new Intent(this, MainActivity.class);
-        startActivity(intent);
-    }
+        EditText etEmail = (EditText) findViewById(R.id.email);
+        v.setEnabled(false);
+        String itemText = etEmail.getText().toString();
+        Toast.makeText(getApplicationContext(), itemText + " logged in", Toast.LENGTH_SHORT).show();
+        }
 
     /*
     public void onCancel (View v) {
