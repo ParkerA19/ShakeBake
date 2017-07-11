e217a46848eaa1bf834b468d27ec13b58a031e64
Parker Andrews <pandrews@fb.com>
2017-07-10 18:17:06 -0700

    added tabs and swipe refresh layout for home and popular

---
+++ app/src/main/java/com/example/pandrews/shakebake/fragments/PopularTimelineFragment.java
@@ -0,0 +1,30 @@
+package com.example.pandrews.shakebake.fragments;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by pandrews on 7/10/17.
+ */
+
+public class PopularTimelineFragment extends RecipesListFragment {
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        populateTimeline();
+    }
+
+//    @Nullable
+//    @Override
+//    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+//        return super.onCreateView(inflater, container, savedInstanceState);
+//    }
+
+    @Override
+    public void populateTimeline() {
+        super.populateTimeline();
+    }
+
+
+}
