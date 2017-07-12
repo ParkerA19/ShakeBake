d50f47775f763ad2b4715052f2074e9385dabe97
jennifer-shin <jenshin@usc.edu>
2017-07-12 15:29:29 -0700

    updated sideactivity

--- app/src/main/java/com/example/pandrews/shakebake/SideActivity.java
+++ app/src/main/java/com/example/pandrews/shakebake/SideActivity.java
@@ -1,9 +1,8 @@
 package com.example.pandrews.shakebake;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
+import android.support.v4.app.FragmentManager;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -67,6 +66,18 @@ public class SideActivity extends AppCompatActivity implements NavigationView.On
         return super.onOptionsItemSelected(item);
     }
 
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        return true;
+    }
+//----uncomment TODO
+//        if (id == R.id.nav_activity_add_recipe) {
+//            fragmentManager.beginTransaction()
+//                    .replace(R.id.content_frame, new AddRecipeActivity()).commit();
 //    @SuppressWarnings("StatementWithEmptyBody")
 //    @Override
 //    public boolean onNavigationItemSelected(MenuItem item) {
@@ -95,6 +106,14 @@ public class SideActivity extends AppCompatActivity implements NavigationView.On
 //        }
 
 //        if (id == R.id.nav_add_a_recipe) {
+//            fragmentManager.beginTransaction()
+//                    .replace(R.id.content_frame, new MyForksActivity()).commit();
+//        } else if (id == R.id.nav_my_forks) {
+//            fragmentManager.beginTransaction()
+//                    .replace(R.id.content_frame, new MyForksActivity()).commit();
+//        } else if (id == R.id.nav_settings) {
+//            fragmentManager.beginTransaction()
+//                    .replace(R.id.content_frame, new SettingsActivity()).commit();
 //
 //        } else if (id == R.id.nav_my_forks) {
 //
@@ -107,9 +126,5 @@ public class SideActivity extends AppCompatActivity implements NavigationView.On
 //        }
 //
 
-    @Override
-    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-        return false;
-    }
 
 }
