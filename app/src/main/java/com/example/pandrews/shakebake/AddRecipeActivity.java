b5965d361acdd420f623516fee3b9552267a7844
Andrea Garcia <andreagarcia@fb.com>
2017-08-07 17:10:40 -0700

    images

--- app/src/main/java/com/example/pandrews/shakebake/AddRecipeActivity.java
+++ app/src/main/java/com/example/pandrews/shakebake/AddRecipeActivity.java
@@ -1,19 +1,15 @@
 package com.example.pandrews.shakebake;
 
-import android.content.ComponentName;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.content.pm.ResolveInfo;
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
-import android.os.Environment;
-import android.os.Parcelable;
 import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.ViewGroup;
@@ -25,17 +21,15 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
 
-import java.io.File;
-import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -76,6 +70,11 @@ public class AddRecipeActivity extends AppCompatActivity {
     Uri videoUri;
     String videoUriString;
 
+    String arrayActions[] = new String[2];
+
+    static Context mContext;
+
+
     //variables for step:video dictionary
     public HashMap<String, String> stepVideo;
 
@@ -86,9 +85,14 @@ public class AddRecipeActivity extends AppCompatActivity {
         setContentView(R.layout.activity_add_a_recipe);
         ButterKnife.bind(this);
 
+
+        arrayActions[0] = "Camera";
+        arrayActions[1] = "Gallery";
+
         //keyboard only pops up when user clicks into an EditText
         this.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
 
+        mContext = getApplicationContext();
         context = getApplicationContext();
 
         supplyList = new ArrayList<>();
@@ -110,13 +114,6 @@ public class AddRecipeActivity extends AppCompatActivity {
         Integer REQUEST_CODE = 21;
         Intent i = new Intent(this, AddStepsActivity.class);
         startActivityForResult(i, REQUEST_CODE);
-    }
-
-    public void onAddImage(View v) {
-//        Integer REQUEST_CODE = 23;
-//        Intent i = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-//        startActivityForResult(i, REQUEST_CODE);
-        openImageIntent();
     }
 
     @Override
@@ -153,28 +150,23 @@ public class AddRecipeActivity extends AppCompatActivity {
 
                 llSteps.addView(createNewStep(step, stepsCount));
 
-            } else if (requestCode == 30) {
-                final boolean isCamera;
-                if (data != null) { //camera was not used
-                    targetUri = data.getData();
+            } else if (requestCode == 23) {
+                targetUri = data.getData();
 
-                    Bitmap bitmap;
-                    try {
-                        bitmap = BitmapFactory.decodeStream(getContentResolver().openInputStream(targetUri));
-                        ivPicture.setImageBitmap(bitmap);
-                        //ivPicture.setImageURI(selectedImageUri);
-                    } catch (FileNotFoundException e) {
-                        e.printStackTrace();
-                        Toast.makeText(context, "set picture failed", Toast.LENGTH_SHORT).show();
-                    }
-                } else {
-                    targetUri = data.getData();
-                    ivPicture.setImageURI(targetUri);
-                }
+                Glide
+                        .with(this)
+                        .load(targetUri)
+                        .asBitmap()
+                        .into(ivPicture);
+            } else if (requestCode == 24) {
+                Bitmap photo = (Bitmap) data.getExtras().get("data");
+                ivPicture.setImageBitmap(photo);
             }
         }
     }
 
+
+    //publish recipe
     public void onAdd(View v) {
         //check that all fields are filled
         if (validate()) {
@@ -314,41 +306,27 @@ public class AddRecipeActivity extends AppCompatActivity {
         return valid;
     }
 
-    private void openImageIntent() {
-
-        // Determine Uri of camera image to save.
-        final File root = new File(Environment.getExternalStorageDirectory() + File.separator + "MyDir" + File.separator);
-        root.mkdirs();
-        final String fname = "img_" + System.currentTimeMillis() + ".jpg";
-        final File sdImageMainDirectory = new File(root, fname);
-        outputFileUri = Uri.fromFile(sdImageMainDirectory);
-
-        // Camera.
-        final List<Intent> cameraIntents = new ArrayList<Intent>();
-        final Intent captureIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
-        final PackageManager packageManager = getPackageManager();
-        final List<ResolveInfo> listCam = packageManager.queryIntentActivities(captureIntent, 0);
-        for(ResolveInfo res : listCam) {
-            final String packageName = res.activityInfo.packageName;
-            final Intent intent = new Intent(captureIntent);
-            intent.setComponent(new ComponentName(packageName, res.activityInfo.name));
-            intent.setPackage(packageName);
-            intent.putExtra(MediaStore.EXTRA_OUTPUT, outputFileUri);
-            cameraIntents.add(intent);
-        }
-
-        // Filesystem.
-        final Intent galleryIntent = new Intent();
-        galleryIntent.setType("image/*");
-        galleryIntent.setAction(Intent.ACTION_GET_CONTENT);
-
-        // Chooser of filesystem options.
-        final Intent chooserIntent = Intent.createChooser(galleryIntent, "Select Source");
-
-        // Add the camera options.
-        chooserIntent.putExtra(Intent.EXTRA_INITIAL_INTENTS, cameraIntents.toArray(new Parcelable[cameraIntents.size()]));
-
-        startActivityForResult(chooserIntent, 30);
+    public void onAddImage(View v) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(AddRecipeActivity.this);
+        //add title and buttons
+        builder.setTitle("Pick A Source")
+                .setItems(arrayActions, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        if (which == 1) {
+                            Integer REQUEST_CODE = 23;
+                            Intent i = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                            startActivityForResult(i, REQUEST_CODE);
+                        } else {
+                            //handle camera intent and image setting here
+                            Integer REQUEST_CODE = 24;
+                            Intent i = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                            startActivityForResult(i, REQUEST_CODE);
+                        }
+                    }
+                });
+        AlertDialog dialog = builder.create();
+        dialog.show();
     }
 
 }
