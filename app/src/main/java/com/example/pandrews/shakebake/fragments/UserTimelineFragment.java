012f5bf51f9a4fdfc25afbe3b86fdecc75f31749
jennifer-shin <jenshin@usc.edu>
2017-08-08 17:32:47 -0700

    UI

--- app/src/main/java/com/example/pandrews/shakebake/fragments/UserTimelineFragment.java
+++ app/src/main/java/com/example/pandrews/shakebake/fragments/UserTimelineFragment.java
@@ -71,10 +71,7 @@ public class UserTimelineFragment extends RecipesListFragment {
         });
 
         // Configure the refeshing colors
-        swipeContainer.setColorSchemeResources(android.R.color.holo_blue_bright,
-                android.R.color.holo_green_light,
-                android.R.color.holo_orange_light,
-                android.R.color.holo_red_light);
+        swipeContainer.setColorSchemeResources(android.R.color.holo_orange_light);
 
 
         // find the RecyclerView
